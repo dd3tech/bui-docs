7fc1d12bab64635d312a127de35cb5993ab73fe6
tbor00 <tborgob@gmail.com>
2023-03-16 00:26:03 +0100

    fix: titles of cards and icons

--- src/components/FeaturesSection.tsx
+++ src/components/FeaturesSection.tsx
@@ -1,14 +1,47 @@
 import { Circle, Text } from 'dd360-ds'
 import DynamicHeroIcon from 'dd360-ds/DynamicHeroIcon'
 
+const cardsInformation = [
+    {
+        title: 'Accessibility',
+        icon: <DynamicHeroIcon className="text-blue-700" icon="ClipboardCopyIcon" width={18} />,
+        description: undefined
+    },
+    {
+        title: 'Ready for production',
+        icon: <DynamicHeroIcon className="text-blue-700" icon="CogIcon" width={18} />,
+        description: undefined
+    },
+    {
+        title: 'Developer experience',
+        icon: <DynamicHeroIcon className="text-blue-700" icon="CodeIcon" width={18} />,
+        description: undefined
+    },
+    {
+        title: 'High quality components',
+        icon: <DynamicHeroIcon className="text-blue-700" icon="SparklesIcon" width={18} />,
+        description: undefined
+    },
+    {
+        title: 'Customization',
+        icon: <DynamicHeroIcon className="text-blue-700" icon="AdjustmentsIcon" width={18} />,
+        description: undefined
+    },
+    {
+        title: 'Solid API and Documentation',
+        icon: <DynamicHeroIcon className="text-blue-700" icon="CheckCircleIcon" width={18} />,
+        description: undefined
+    }
+]
+
 const FeaturesSection = () => (
     <section
         className="grid gap-x-14 gap-y-12 pt-40 p-4 sm:px-8 m-auto"
         style={{ gridTemplateColumns: 'repeat(auto-fit, minmax(295px, 1fr))', maxWidth: 1061 }}
     >
-        {[1, 2, 3, 4, 5, 6].map((item) => (
+        {cardsInformation.map((item) => (
             <article
-                key={item}
+                key={item.title}
                 style={{
                     background: 'rgba(255, 255, 255, 0.2)',
                     backdropFilter: 'blur(10px)',
@@ -20,10 +53,10 @@ const FeaturesSection = () => (
             >
                 <div className="flex items-center mb-6">
                     <Circle width="32px" height="32px" backgroundColor="#DBEAFE">
-                        <DynamicHeroIcon className="text-blue-700" icon="ClipboardCopyIcon" width={18} />
+                        {item.icon}
                     </Circle>
-                    <Text className="font-semibold ml-2" size={14}>
-                        Accessibility
+                    <Text className="font-semibold ml-2" size={10}>
+                        {item.title}
                     </Text>
                 </div>
                 <Text variant="p">
