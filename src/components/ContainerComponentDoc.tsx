857981c3dc2ca2321769011e9a6138393eb2e2b0
Lautaro Vilas <lautaro.vilas@dd360tech.mx>
2023-03-21 15:54:50 -0300

    feat: add docs form, and create ContainerComponentDoc

---
+++ src/components/ContainerComponentDoc.tsx
@@ -0,0 +1,14 @@
+import { FC, ReactNode } from 'react'
+import { composeClasses } from 'dd360-ds/lib'
+
+interface Props {
+    children: ReactNode
+    className: string
+}
+
+const ContainerComponentDoc: FC<Props> = ({ children, className }) => {
+    const classContainer = composeClasses(`py-5 base:px-1 lg:px-3 rounded-md my-6 mx-0 bg-gray-100 ${className}`)
+    return <div className={classContainer}>{children}</div>
+}
+
+export default ContainerComponentDoc
