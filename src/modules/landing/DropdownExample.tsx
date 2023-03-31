96c02fb20581aec0f2142c84deea15f12507daa1
tbor00 <tborgob@gmail.com>
2023-03-31 17:47:13 -0300

    ref: change array map to improve visibility

--- src/modules/landing/DropdownExample.tsx
+++ src/modules/landing/DropdownExample.tsx
@@ -1,44 +1,42 @@
 import { useState } from 'react'
 import { Card, Button, Divider } from 'dd360-ds'
-import DynamicHeroIcon from 'dd360-ds/DynamicHeroIcon'
+import DynamicHeroIcon, { IconName } from 'dd360-ds/DynamicHeroIcon'
 import { composeClasses, StyleObject } from 'dd360-ds/lib'
 
-const POSITIONS = {
-    one: 1,
-    two: 2,
-    three: 3
-}
+const DROPDOWN_ITEMS: Array<{ title: string; iconName: IconName }> = [
+    {
+        title: 'Shift Up',
+        iconName: 'ArrowUpIcon'
+    },
+    {
+        title: 'Shift right',
+        iconName: 'ArrowRightIcon'
+    },
+    {
+        title: 'Shift left',
+        iconName: 'ArrowLeftIcon'
+    }
+]
 
 const DropdownExample = ({ style, className }: { style: StyleObject; className: string }) => {
     const [position, setPosition] = useState(1)
 
     return (
         <Card padding={0} rounded="md" width={170} style={style} className={composeClasses('p-2', className)}>
-            <Button className="text-xs font-medium w-full text-red-500 flex items-center gap-2" variant="link">
+            <Button className="text-xs font-medium w-full text-red-500 flex items-center gap-2 hover:bg-red-100" variant="link">
                 <DynamicHeroIcon icon="TrashIcon" width={17} /> Delete concept
             </Button>
             <Divider className="border-gray-300" />
-            <Button
-                className="text-xs font-medium w-full mt-2 py-1 flex items-center gap-2"
-                onClick={() => setPosition(POSITIONS.one)}
-                variant={position === POSITIONS.one ? 'primary' : 'link'}
-            >
-                <DynamicHeroIcon icon="ArrowRightIcon" width={17} /> Shift right
-            </Button>
-            <Button
-                className="text-xs font-medium w-full mt-2 py-1 flex items-center gap-2"
-                onClick={() => setPosition(POSITIONS.two)}
-                variant={position === POSITIONS.two ? 'primary' : 'link'}
-            >
-                <DynamicHeroIcon icon="ArrowRightIcon" width={17} /> Shift right
-            </Button>
-            <Button
-                className="text-xs font-medium w-full mt-2 py-1 flex items-center gap-2"
-                onClick={() => setPosition(POSITIONS.three)}
-                variant={position === POSITIONS.three ? 'primary' : 'link'}
-            >
-                <DynamicHeroIcon icon="ArrowLeftIcon" width={17} /> Shift left
-            </Button>
+            {DROPDOWN_ITEMS.map(({ title, iconName }, idx) => (
+                <Button
+                    key={idx}
+                    className="text-xs font-medium w-full mt-2 py-1 flex items-center gap-2"
+                    onClick={() => setPosition(idx)}
+                    variant={position === idx ? 'primary' : 'link'}
+                >
+                    <DynamicHeroIcon icon={iconName} width={17} /> {title}
+                </Button>
+            ))}
         </Card>
     )
 }
