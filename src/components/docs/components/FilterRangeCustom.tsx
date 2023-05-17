7a8d7f0389488bdaf7777f844405635d5fc40c80
Misael840 <128083295+Misael840@users.noreply.github.com>
2023-05-17 09:56:36 -0600

    Feat(Documentation): add filter range and images documentation (#24)
    * feat: add filter range and images documentation
    
    * [ci]: format code

---
+++ src/components/docs/components/FilterRangeCustom.tsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from 'react'
+import { FilterRange, Text } from 'dd360-ds'
+import DynamicHeroIcon from 'dd360-ds/DynamicHeroIcon'
+
+const FilterRangeCustom = ({
+  onTop,
+  minMax
+}: {
+  onTop: boolean
+  minMax: boolean
+}) => {
+  const [domLoaded, setDomLoaded] = useState<boolean>(false)
+  const [position, setPosition] = useState<{
+    top: number
+    left: number
+    show: boolean
+  }>({
+    top: 0,
+    left: 0,
+    show: false
+  })
+
+  useEffect(() => {
+    setDomLoaded(true)
+  }, [])
+
+  return (
+    <>
+      {domLoaded && (
+        <div>
+          <button
+            className="flex items-center gap-1"
+            onClick={(event) => {
+              setPosition((position) => ({
+                top: event.pageY,
+                left: event.pageX,
+                show: !position.show
+              }))
+            }}
+          >
+            <Text size="lg" bold>
+              {onTop ? 'Filter range on top' : 'Filter range'}
+            </Text>
+            <DynamicHeroIcon icon="FilterIcon" className="w-4 h-4" />
+          </button>
+          <FilterRange
+            max={minMax ? 100 : 0}
+            min={minMax ? 30 : 0}
+            onApply={() => undefined}
+            onReset={() => undefined}
+            position={{
+              top: position.top + (onTop ? -180 : 40),
+              left: position.left,
+              show: position.show
+            }}
+            title="Filter range name"
+            defaultMax={minMax ? 100 : 0}
+            defaultMin={minMax ? 30 : 0}
+            textMax={minMax ? 'Custom max text' : 'Maximum'}
+            textMin={minMax ? 'Custom min text' : 'Minimum'}
+          />
+        </div>
+      )}
+    </>
+  )
+}
+
+export default FilterRangeCustom
---
+++ src/components/docs/components/index.ts
@@ -0,0 +1,3 @@
+export { default as CustomCallout } from './CustomCallout'
+export { default as FilterRangeCustom } from './FilterRangeCustom'
+export { default as FilterRangeSliderCustom } from './FilterRangeSliderCustom'
