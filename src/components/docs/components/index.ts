4fb26f7909ca449194b8b1382575db7b6d4af2d6
misael.arreola <misael.arreola@dd3tech.mx>
2023-05-19 10:54:37 -0600

    feat: add portal documentation

--- src/components/docs/components/index.ts
+++ src/components/docs/components/index.ts
@@ -1,3 +1,4 @@
 export { default as CustomCallout } from './CustomCallout'
 export { default as FilterRangeCustom } from './FilterRangeCustom'
 export { default as FilterRangeSliderCustom } from './FilterRangeSliderCustom'
+export { default as PortalCustom } from './PortalCustom'
