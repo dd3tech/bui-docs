4d230798b81452a6148f820c89622f5a9de64c0d
misael.arreola <misael.arreola@dd3tech.mx>
2023-05-19 12:10:00 -0600

    refactor: run format

--- src/components/docs/components/PortalCustom.tsx
+++ src/components/docs/components/PortalCustom.tsx
@@ -3,20 +3,24 @@ import { Button, Text } from 'dd360-ds'
 import { Portal } from 'dd360-ds/Portal'
 
 const PortalCustom = () => {
-    const [isactive, setIsactive] = useState<boolean>(false)
-    const [domLoaded, setDomLoaded] = useState<boolean>(false)
+  const [isactive, setIsactive] = useState<boolean>(false)
+  const [domLoaded, setDomLoaded] = useState<boolean>(false)
 
-    useEffect(() => {
-        setDomLoaded(true)
-    }, [])
+  useEffect(() => {
+    setDomLoaded(true)
+  }, [])
 
   return (
-    <div className='relative flex items-center gap-4'>
-      <Text size='base' bold>Portal component</Text>
-      <Button onClick={()=> setIsactive((isactive) => !isactive)}>Toggle Portal</Button>
+    <div className="relative flex items-center gap-4">
+      <Text size="base" bold>
+        Portal component
+      </Text>
+      <Button onClick={() => setIsactive((isactive) => !isactive)}>
+        Toggle Portal
+      </Button>
       {domLoaded && isactive && (
         <Portal>
-          <div className='fixed left-1/2 top-1/2 w-40 h-24 flex justify-center items-center border rounded-lg bg-white'>
+          <div className="fixed left-1/2 top-1/2 w-40 h-24 flex justify-center items-center border rounded-lg bg-white">
             <Text>Hey!!! I am a Portal</Text>
           </div>
         </Portal>
