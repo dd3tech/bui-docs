a281ce0ccb00bf1d698a99858e026339a0d4a493
misael.arreola <misael.arreola@dd3tech.mx>
2023-05-31 18:48:05 -0600

    fix: add animation list from library and implement flex component

--- src/components/Layout/TransitionCustom.tsx
+++ src/components/Layout/TransitionCustom.tsx
@@ -1,71 +1,34 @@
 import { useState } from 'react'
 import {
+  ANIMATION_LIST,
   AnimationsTypes,
   Button,
   Checkbox,
+  Flex,
   FormControlLabel,
   Select,
   Text,
   Transition
 } from 'dd360-ds'
 
-const ANIMATION_LIST = {
-  hoverTranlationLeft: { label: 'hover-tranlation-left' },
-  hoverTranlationRight: { label: 'hover-tranlation-right' },
-  hoverTranlationUp: { label: 'hover-tranlation-up' },
-  hoverTranlationDown: { label: 'hover-tranlation-down' },
-  hoverScale: { label: 'hover-scale' },
-  fadeIn: { label: 'fade-in' },
-  fadeInLeft: { label: 'fade-in-left' },
-  fadeInRight: { label: 'fade-in-right' },
-  fadeInDown: { label: 'fade-in-down' },
-  fadeInUp: { label: 'fade-in-up' },
-  fadeOut: { label: 'fade-out' },
-  fadeOutLeft: { label: 'fade-out-left' },
-  fadeOutRight: { label: 'fade-out-right' },
-  fadeOutDown: { label: 'fade-out-down' },
-  fadeOutUp: { label: 'fade-out-up' },
-  slideInLeft: { label: 'slide-in-left' },
-  slideInRight: { label: 'slide-in-right' },
-  slideInDown: { label: 'slide-in-down' },
-  slideInUp: { label: 'slide-in-up' },
-  slideOutLeft: { label: 'slide-out-left' },
-  slideOutRight: { label: 'slide-out-right' },
-  slideOutDown: { label: 'slide-out-down' },
-  slideOutUp: { label: 'slide-out-up' },
-  zoomIn: { label: 'zoom-in' },
-  zoomOut: { label: 'zoom-out' },
-  zoomInLeft: { label: 'zoom-in-left' },
-  zoomInRight: { label: 'zoom-in-right' },
-  zoomInDown: { label: 'zoom-in-down' },
-  zoomInUp: { label: 'zoom-in-up' },
-  zoomOutLeft: { label: 'zoom-out-left' },
-  zoomOutRight: { label: 'zoom-out-right' },
-  zoomOutDown: { label: 'zoom-out-down' },
-  zoomOutUp: { label: 'zoom-out-up' },
-  flipInX: { label: 'flip-in-x' },
-  flipInY: { label: 'flip-in-y' },
-  flipOutX: { label: 'flip-out-x' },
-  flipOutY: { label: 'flip-out-y' },
-  lightSpeedInLeft: { label: 'light-speed-in-left' },
-  lightSpeedOutLeft: { label: 'light-speed-out-left' },
-  lightSpeedInRight: { label: 'light-speed-in-right' },
-  lightSpeedOutRight: { label: 'light-speed-out-right' },
-  rotateLeft: { label: 'rotate-left' },
-  rotateRight: { label: 'rotate-right' },
-  rotateLeftBack: { label: 'rotate-left-back' },
-  rotateRightBack: { label: 'rotate-right-back' },
-  rotateLeftFade: { label: 'rotate-left-fade' },
-  rotateRightFade: { label: 'rotate-right-fade' },
-  rotateLeftBackFade: { label: 'rotate-left-back-fade' },
-  rotateRightBackFade: { label: 'rotate-right-back-fade' },
-  bounce: { label: 'bounce' },
-  rubberBand: { label: 'rubber-band' },
-  heartBeat: { label: 'heart-beat' },
-  swing: { label: 'swing' },
-  spin: { label: 'spin' },
-  blink: { label: 'blink' }
+const convertAnimationValues = (
+  animationList: Record<string, string>
+): Record<string, { label: string }> => {
+  const convertedList: Record<string, { label: string }> = {}
+
+  for (const key in animationList) {
+    if (animationList.hasOwnProperty(key)) {
+      const originalValue = animationList[key]
+      convertedList[key] = {
+        label: originalValue
+      }
+    }
+  }
+
+  return convertedList
 }
+
+const SELECT_ANIMATION_LIST = convertAnimationValues(ANIMATION_LIST)
 
 const VisibilityExample = () => {
   const [isActive, setIsActive] = useState<boolean>(false)
@@ -74,9 +37,12 @@ const VisibilityExample = () => {
     <>
       <Button onClick={() => setIsActive(!isActive)}>Toggle animation</Button>
       <Transition show={isActive}>
-        <div className="w-24 h-24 flex items-center text-center text-white bg-red-500 rounded-lg">
+        <Flex
+          alignItems="center"
+          className="w-24 h-24 text-center text-white bg-red-500 rounded-lg"
+        >
           Fade animation
-        </div>
+        </Flex>
       </Transition>
     </>
   )
@@ -89,9 +55,13 @@ const DurationDelayExample = () => {
     <>
       <Button onClick={() => setIsActive(!isActive)}>Toggle animation</Button>
       <Transition show={isActive} duration={500} delay={100}>
-        <div className="w-24 h-24 flex items-center text-center text-white bg-red-500 rounded-lg">
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          className="w-24 h-24 text-center text-white bg-red-500 rounded-lg"
+        >
           Duration & delay
-        </div>
+        </Flex>
       </Transition>
     </>
   )
@@ -108,9 +78,13 @@ const ControlAnimationExample = () => {
         animationStart="fadeInRight"
         animationEnd="flipOutY"
       >
-        <div className="w-24 h-24 flex items-center text-center text-white bg-red-500 rounded-lg">
+        <Flex
+          alignItems="center"
+          justifyContent="center"
+          className="w-24 h-24 text-center text-white bg-red-500 rounded-lg"
+        >
           Hey!
-        </div>
+        </Flex>
       </Transition>
     </>
   )
@@ -123,14 +97,18 @@ const AllAnimationsPanel = () => {
   const [animationEnd, setAnimationEnd] = useState<string>('fade-out')
 
   return (
-    <div className="w-full p-8 flex flex-col-reverse md:flex-row 2xl:flex-col gap-8 items-center">
-      <div className="w1/2 flex flex-col 2xl:flex-row gap-4">
-        <div className="w-full flex gap-4 flex-col 2xl:flex-row">
+    <Flex
+      gap="8"
+      alignItems="center"
+      className="w-full p-8 flex-col-reverse md:flex-row 2xl:flex-col"
+    >
+      <Flex gap="4" className="w-1/2 flex-col 2xl:flex-row">
+        <Flex gap="4" className="w-full flex-col 2xl:flex-row">
           <div>
             <Text>First animation</Text>
             <Select
               value={animationStart}
-              optionsList={ANIMATION_LIST}
+              optionsList={SELECT_ANIMATION_LIST}
               onChange={(e) => setAnimationStart(e.target.value)}
             />
           </div>
@@ -138,12 +116,12 @@ const AllAnimationsPanel = () => {
             <Text>Last animation</Text>
             <Select
               value={animationEnd}
-              optionsList={ANIMATION_LIST}
+              optionsList={SELECT_ANIMATION_LIST}
               onChange={(e) => setAnimationEnd(e.target.value)}
             />
           </div>
-        </div>
-        <div className="w-full flex flex-col 2xl:flex-row md:items-end gap-4">
+        </Flex>
+        <Flex gap="4" className="w-full flex-col 2xl:flex-row md:items-end">
           <Button
             className="h-12 w-full"
             onClick={() => setIsActive(!isActive)}
@@ -161,21 +139,25 @@ const AllAnimationsPanel = () => {
             labelPlacement="end"
             className="w-full"
           />
-        </div>
-      </div>
-      <div className="w-1/2 h-44 flex items-center justify-center">
+        </Flex>
+      </Flex>
+      <Flex alignItems="center" justifyContent="center" className="w-1/2 h-44">
         <Transition
           show={isActive}
           animationStart={animationStart as AnimationsTypes}
           animationEnd={animationEnd as AnimationsTypes}
           alwaysRender={isAlwaysRender}
         >
-          <div className="w-24 h-24 flex items-center justify-center text-center text-white bg-red-500 rounded-lg">
+          <Flex
+            alignItems="center"
+            justifyContent="center"
+            className="w-24 h-24 text-center text-white bg-red-500 rounded-lg"
+          >
             Hey!
-          </div>
+          </Flex>
         </Transition>
-      </div>
-    </div>
+      </Flex>
+    </Flex>
   )
 }
 
