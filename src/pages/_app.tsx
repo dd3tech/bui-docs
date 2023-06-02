0c5d850b9816eadc5ccc546eeaebd3458888d24c
tbor00 <tborgob@gmail.com>
2023-06-02 13:17:55 -0300

    fix: homologate opengraph

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,19 @@
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import { appWithTranslation } from 'next-i18next'
+import { appWithTranslation, useTranslation } from 'next-i18next'
 import { ThemeProvider } from 'dd360-ds/theme'
 import NextNProgress from 'nextjs-progressbar'
-import { Layout } from '@/components'
+import { Layout, OpenGraph } from '@/components'
 
 import 'dd360-ds/dd360.css'
 import '@/theme/global.css'
 
 function App({ Component, pageProps }: AppProps) {
+  const { t } = useTranslation('common')
+
   return (
     <>
       <NextNProgress color="#1d4ed8" />
-      <Head>
-        <title>DD360 DS</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
+      <OpenGraph title={t('metadata.home')!} />
       <Layout>
         <ThemeProvider>
           <Component {...pageProps} />
