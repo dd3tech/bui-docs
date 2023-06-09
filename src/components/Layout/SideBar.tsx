ac32d571e44656c0879639782bf5692b118a59fb
lautarovilas <lautaro.vilas@dd3tech.mx>
2023-06-08 23:29:12 -0300

    wip doc componente-bar-list

--- src/components/Layout/SideBar.tsx
+++ src/components/Layout/SideBar.tsx
@@ -286,6 +286,11 @@ const components: ComponentObjectProps = {
         badge: BADGE_TYPES.new
       },
       {
+        label: 'BarList',
+        pathname: 'bar-list',
+        badge: BADGE_TYPES.new
+      },
+      {
         label: 'Skeleton',
         pathname: 'skeleton'
       },
