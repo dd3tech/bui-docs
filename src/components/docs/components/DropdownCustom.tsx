2539f972a0a0fe7ad255989e7113076564b7cf65
lautarovilas <lautarovilas@users.noreply.github.com>
2023-06-17 01:36:07 +0000

    [ci]: format code

--- src/components/docs/components/DropdownCustom.tsx
+++ src/components/docs/components/DropdownCustom.tsx
@@ -1,31 +1,35 @@
-import { useEffect, useState } from "react"
-import { Dropdown, Button, Text, Skeleton } from "dd360-ds"
+import { useEffect, useState } from 'react'
+import { Dropdown, Button, Text, Skeleton } from 'dd360-ds'
 
-const DropdownCustom = ({ disableAnimation = false }: { disableAnimation?: boolean }) => {
+const DropdownCustom = ({
+  disableAnimation = false
+}: {
+  disableAnimation?: boolean
+}) => {
   const [domLoaded, setDomLoaded] = useState<boolean>(false)
 
-  useEffect(() => setDomLoaded(true),[])
+  useEffect(() => setDomLoaded(true), [])
 
   return (
     <div className="w-64 px-2">
-      {domLoaded ?
+      {domLoaded ? (
         <Dropdown disableAnimation={disableAnimation}>
           <Dropdown.Trigger>
-              <Button className="h-10 pt-2 pb-3" paddingX="3">
-                <Text size="xs" bold className="text-white">
-                  Trigger
-                </Text>
-              </Button>
+            <Button className="h-10 pt-2 pb-3" paddingX="3">
+              <Text size="xs" bold className="text-white">
+                Trigger
+              </Text>
+            </Button>
           </Dropdown.Trigger>
           <Dropdown.Menu>
-              <Text className="px-5" size="base" bold>
-                Content
-              </Text>
+            <Text className="px-5" size="base" bold>
+              Content
+            </Text>
           </Dropdown.Menu>
         </Dropdown>
-        : (
-          <Skeleton className="h-10 rounded-md w-14" />
-        )}
+      ) : (
+        <Skeleton className="h-10 rounded-md w-14" />
+      )}
     </div>
   )
 }
