65b9d9f2ccb2bbd9b9c3cb114203c9878e2a4e2d
lautarovilas <127885606+lautarovilas@users.noreply.github.com>
2023-08-04 14:37:33 -0300

    feat: implemented intersection observer for floatingNav (#62)
    * feat: add custom hook useIntersectionObserver and implemented in floatingNav
    
    * [ci]: format code
    
    ---------
    
    Co-authored-by: lautarovilas <lautarovilas@users.noreply.github.com>

--- src/components/Layout/FloatingNav.tsx
+++ src/components/Layout/FloatingNav.tsx
@@ -2,6 +2,7 @@ import { useRouter } from 'next/router'
 import { Text } from 'dd360-ds'
 import { composeClasses } from 'dd360-ds/lib'
 import { useTheme } from '@/store/theme-store'
+import { useIntersectionObserver } from '@/hooks/useIntersectionObserver'
 
 export type Entries = {
   label: string
@@ -22,6 +23,11 @@ const FloatingNav = ({ entries }: IFloatingNav) => {
 
   const isLight = useTheme().isLightTheme
 
+  const visibleElementIndex = useIntersectionObserver({
+    targetSearchElement: 'floating-nav',
+    searchBy: 'name'
+  })
+
   const handleClick = (position: number, id: string) => {
     document.getElementById('layout-content')?.scrollTo({
       behavior: 'smooth',
@@ -38,30 +44,34 @@ const FloatingNav = ({ entries }: IFloatingNav) => {
           extendedPalette.cardBorderColor
         )}
       >
-        {entries?.map((entry) => (
-          <Text
-            key={`floating-nav-${entry.label}`}
-            variant="span"
-            className={composeClasses(
-              entry.isActive
-                ? `font-bold ${extendedPalette.tertiaryTextActive}`
-                : extendedPalette.tertiaryText,
-              isLight ? 'hover:font-bold' : 'hover:text-white',
-              'cursor-pointer'
-            )}
-            onClick={() => handleClick(entry.position, entry.id)}
-          >
-            {entry.isActive && (
-              <span
-                className={composeClasses(
-                  isLight ? 'bg-black' : 'bg-white',
-                  '-left-0.5 w-0.5 h-5 absolute'
-                )}
-              ></span>
-            )}
-            {entry.label}
-          </Text>
-        ))}
+        {entries?.map((entry, index) => {
+          const isInView = visibleElementIndex === index
+
+          return (
+            <Text
+              key={`floating-nav-${entry?.label}`}
+              variant="span"
+              className={composeClasses(
+                isInView
+                  ? `font-bold ${extendedPalette.tertiaryTextActive}`
+                  : extendedPalette.tertiaryText,
+                isLight ? 'hover:font-bold' : 'hover:text-white',
+                'cursor-pointer'
+              )}
+              onClick={() => handleClick(entry?.position, entry?.id)}
+            >
+              {isInView && (
+                <span
+                  className={composeClasses(
+                    isLight ? 'bg-black' : 'bg-white',
+                    '-left-0.5 w-0.5 h-5 absolute'
+                  )}
+                ></span>
+              )}
+              {entry?.label}
+            </Text>
+          )
+        })}
       </div>
     </>
   )
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,5 +6,18 @@ export const splitArray = (array: any[], chunkSize: number) => {
   return partes
 }
 
+export const getElements = (
+  targetSearchElement: string,
+  searchBy: 'name' | 'class' | 'tag'
+): HTMLElement[] => {
+  const assignedSearchMethod = {
+    name: document?.getElementsByName(targetSearchElement),
+    class: document?.getElementsByClassName(targetSearchElement),
+    tag: document?.getElementsByTagName(targetSearchElement)
+  }
+
+  return Array.from(assignedSearchMethod[searchBy]) as HTMLElement[]
+}
+
 export const isProd = process.env.NODE_ENV === 'production'
 export const isDevelopment = process.env.NODE_ENV === 'development'
