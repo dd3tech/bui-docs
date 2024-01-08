6af2ae550c3d8b1f0518d31518450df4ae32827d
tbor00 <timoteo.borgobello@dd3tech.mx>
2024-01-08 10:39:03 -0300

    fix: change builder landing page url

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,9 +7,9 @@ export const NPM_URL = 'https://www.npmjs.com/package/dd360-ds'
 // sites
 export const BUI_URL = 'https://bui.dd360.mx'
 export const SIMULATOR_URL = 'https://onboarding.dd360.mx'
-export const BUILDD3R_LANDING = 'https://buildd3r.dd360.mx'
 export const APP_DD360 = 'https://app.dd360.mx'
 export const LANDING_DD360 = 'https://dd360.mx'
+export const BUILDD3R_LANDING = `${LANDING_DD360}/platform`
 
 // Theme
 export const THEMES = { dark: 'dark', light: 'light' }
